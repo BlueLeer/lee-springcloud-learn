19a68d6dd6a74e13f6d4a62b771a5291b2f381bc
BlueLeer <251668577@qq.com>
2020-01-07 01:20:32 +0800

    代码更新

--- hello-eureka-client-service-consumer-feign/src/main/java/com/lee/springcloud/controller/HiController.java
+++ hello-eureka-client-service-consumer-feign/src/main/java/com/lee/springcloud/controller/HiController.java
@@ -2,7 +2,10 @@ package com.lee.springcloud.controller;
 
 import com.lee.springcloud.service.HiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,15 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/7/17 17:27
  * @description
  */
-@RestController
+@Controller
 public class HiController {
 
     @Autowired
     private HiService hiService;
 
     @GetMapping(value = "/hi")
+    @ResponseBody
     public String hi() {
         return hiService.hi();
+    }
+    
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
     }
 
 }
